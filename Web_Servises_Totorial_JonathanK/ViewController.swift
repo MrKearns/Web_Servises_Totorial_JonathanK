3f23b733e0579b1432e44a70c05b215221b302e0
MrKearns <jkearns631@students.mchenry.edu>
2023-02-27 11:19:06 -0600

    first

--- Web_Servises_Totorial_JonathanK/ViewController.swift
+++ Web_Servises_Totorial_JonathanK/ViewController.swift
@@ -11,9 +11,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        if let url = NSURL(string: "https://api.darksky.net/forecast/ae58c5fa7285b492f6a553d200018d9e/42.231750,-88.333549") {
+            if let data = NSData(contentsOf: url as URL) {
+            
+                do{
+                    let parsed = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String: AnyObject]
+                    
+                    let newDict = parsed
+                    
+                    
+                    print(newDict["currently"]!["summary : icon"] as Any)
+                    print(newDict["currently"]!["temperature"] as Any)
+                    print(newDict["currently"]!["dewPoint"] as Any)
+                    
+                    self.currentTemp.text = "\(newDict["currently"]!["temperature"]!!) °F"
+                    self.currentSummary.text = "\(newDict["minutely"]!["summary"]!!)"
+                    self.currentDewpoint.text = "\(newDict["currently"]!["dewPoint"]!!) °F"
+                    
+                    self.timezone.text = "\(newDict["timezone"]!)"
+                    
+//                    self.weatherIcon.image = UIImage(named: "\(newDict["minutely"]!["icon"]!!)")
+                    
+                }
+                catch let error as NSError{
+                    print("A JSON error acurred, here are the details:\n \(error)")
+                }
+            }
+        }
     }
-
-
+    
+    @IBOutlet weak var currentTemp: UILabel!
+    
+    @IBOutlet weak var currentSummary: UILabel!
+    
+    @IBOutlet weak var currentDewpoint: UILabel!
+    
+    @IBOutlet weak var timezone: UILabel!
+    
+    @IBOutlet weak var weatherIcon: UIImageView!
 }
 
